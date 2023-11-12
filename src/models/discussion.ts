7b6c0a1d24d6792327d8443617022b08aeb2644d
fadhilAmri <fadhil133113@gmail.com>
2023-11-13 02:05:05 +0700

    feat: add models for addDiscussion and getAllDiscussions

--- src/models/discussion.ts
+++ src/models/discussion.ts
@@ -1,7 +1,25 @@
-// import {
-//     BaseEntity,
-//     Column,
-//     Entity,
-//     OneToMany,
-//     PrimaryGeneratedColumn,
-// } from "typeorm";
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient();
+
+export class DiscussionModel
+{
+    async getAllDiscussions(){
+        const result = await prisma.diskusi.findMany();
+        return result; 
+    }
+    
+    async addNewDiscussion(judul: string, author:string, content:string, numOfComment:number, keywords:string){
+        const discussion = await prisma.diskusi.create({
+            data:{
+                Judul : judul,
+                Penulis : author,
+                Konten : content,
+                JumlahKomentar : numOfComment,
+                Keywords : keywords,
+                },
+            })
+        return discussion;
+    }
+}
+
