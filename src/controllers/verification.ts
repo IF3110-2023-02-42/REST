f3a92fa001ee3b52075f376d38c81f2e5698bc95
fadhilAmri <fadhil133113@gmail.com>
2023-11-16 00:34:53 +0700

    fix: fix soap handler for accepting params case

--- src/controllers/verification.ts
+++ src/controllers/verification.ts
@@ -6,32 +6,6 @@ export class VerificationController {
     private url: string = process.env.SOAP_BASE_ENDPOINT ?? "http://localhost:6060/api";
     findAll() {
       return async (req: Request, res: Response) => {
-          // Nanti minta ke SOAP list semua request verifikasi
-
-          // let result = [
-          //     {
-          //       id: "1",
-          //       nama: "Roberto Santan",
-          //       email: "robsantan@gmail.com",
-          //       tanggalPengajuan: "10 November 2023",
-          //       status: "unverified",
-          //     }, 
-          //     {
-          //       id: "2",
-          //       nama: "Roberto Santani",
-          //       email: "robi@gmail.com",
-          //       tanggalPengajuan: "10 November 2023",
-          //       status: "accepted",
-          //     },
-          //     {
-          //       id: "3",
-          //       nama: "Roberto Santano",
-          //       email: "robo@gmail.com",
-          //       tanggalPengajuan: "10 November 2023",
-          //       status: "rejected",
-          //     },
-          //   ];
-
           let result = await soapHandler(this.url, "getAllRequest")
 
           res.status(StatusCodes.OK).json({
@@ -48,13 +22,7 @@ export class VerificationController {
           let id = req.body.id;
           let newStatus = req.body.newStatus;
 
-          // let result = {
-          //   id : id,
-          //   newStatus : newStatus,
-          // };
-
           let params = [id, newStatus];
-
           let result = await soapHandler(this.url, "updateStatus", params)
   
           res.status(StatusCodes.OK).json({
